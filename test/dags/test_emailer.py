8eaa412ab9c9ac0ff85564a24b5c48a964848df7
Chester Asuncion <asuncionmcv@gmail.com>
2024-03-19 19:53:09 +0800

    add test for emailer

--- test/dags/test_emailer.py
+++ test/dags/test_emailer.py
@@ -1,1 +1,13 @@
-import airflow.models.dagbag
+from airflow.models import DagBag, DAG
+from dags.emailer import emailer
+
+dag = emailer
+
+def test_dag_should_exist():
+    expected_id = dag.dag_id 
+    
+    assertion = 'dag should exist'
+    assert emailer.dag_id == expected_id, assertion
+
+def dag_should_have_no_cycle():
+    return

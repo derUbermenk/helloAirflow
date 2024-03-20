7c34e15964869ff3bfbf53b4eecf2f908570c264
Chester Asuncion <asuncionmcv@gmail.com>
2024-03-20 19:46:34 +0800

    added tests for emailer dag

--- dags/emailer.py
+++ dags/emailer.py
@@ -1,9 +1,23 @@
 import datetime
-
 from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
 
 emailer = DAG(
     dag_id="emailer",
     start_date=datetime.datetime(2021,1,1),
     schedule="@daily"
 )
+
+format_emails = DockerOperator(
+    task_id = "format_emails",
+    image = "format_emails:latest",
+    dag=emailer
+) 
+
+pseudo_send_emails = DockerOperator(
+    task_id = "pseudo_send_emails",
+    image = "pseudo_send_emails:latest",
+    dag=emailer
+)
+
+format_emails >> pseudo_send_emails 
--- test/dags/test_emailer.py
+++ test/dags/test_emailer.py
@@ -1,13 +1,35 @@
-from airflow.models import DagBag, DAG
+import pytest 
 from dags.emailer import emailer
+from airflow.utils.dag_cycle_tester import check_cycle 
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.exceptions import AirflowDagCycleException
 
 dag = emailer
 
 def test_dag_should_exist():
     expected_id = dag.dag_id 
     
-    assertion = 'dag should exist'
-    assert emailer.dag_id == expected_id, assertion
+    assert emailer.dag_id == expected_id, "dag should exist"
 
-def dag_should_have_no_cycle():
-    return
+def test_dag_tasks():
+    tasks = dag.tasks
+
+
+    task_ids = [task.task_id for task in tasks]
+
+    expected_task_ids = [
+        "format_emails",
+        "pseudo_send_emails"
+    ]
+
+    assert task_ids == expected_task_ids, "dag should have all expected tasks"
+
+def test_dag_should_have_no_cycle():
+    did_not_raise_cycle_error = True 
+
+    try:
+        check_cycle(dag)
+    except AirflowDagCycleException:
+        did_not_raise_cycle_error = False 
+
+    assert did_not_raise_cycle_error, "dag should not have a cycle"

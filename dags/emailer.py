1d61d836a8484cbd57560b0ae735b67816375e9b
Chester Asuncion <asuncionmcv@gmail.com>
2024-04-19 15:21:22 +0800

    Dev/finalize emailer dag (#12)
    * add mounts and command for format and send emails tasks
    
    * add docker compose file for airflow service
    
    * added folders needed by docker tasks
    
    * fix multiple send_emails in dag emailer

--- dags/emailer.py
+++ dags/emailer.py
@@ -3,6 +3,7 @@
 import datetime
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount 
 
 emailer = DAG(
     dag_id="emailer",
@@ -15,13 +16,33 @@ emailer = DAG(
 format_emails = DockerOperator(
     task_id = "format_emails",
     image = "format_emails",
-    dag=emailer
-) 
+    dag=emailer,
+    command = [
+        "format_emails",
+        "./users/users.csv"
+        "{{ds}}",
+        "./emails/{{ds}}_emails.json"
+    ],
+    mounts = [
+        Mount(source="../volumes/emails", target="/emails", type="bind"),
+        Mount(source="../volumes/users", target="/users", type="bind"),
+    ]
+)
 
 send_emails = DockerOperator(
     task_id = "send_emails",
     image = "send_emails",
-    dag=emailer
+    dag=emailer,
+    command = [
+        "send_emails",
+        "{{ds}}",
+        "./emails/{{ds}}_emails.json",
+        "./logs/{{ds}}_logs.log"
+    ],
+    mounts = [
+        Mount(source="../volumes/emails", target="/emails", type="bind"),
+        Mount(source="../volumes/logs", target="/logs", type="bind"),
+    ]
 )
 
-format_emails >> send_emails 
+format_emails >> send_emails
